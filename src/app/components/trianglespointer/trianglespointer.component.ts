0e1c3946a955bf3dbcb40240a460086cb45b07b7
Kurt Starck <kurtstarck@yahoo.com>
2021-11-04 12:13:04 -0500

    click listener added to change color on CSS global

--- src/app/components/trianglespointer/trianglespointer.component.ts
+++ src/app/components/trianglespointer/trianglespointer.component.ts
@@ -13,8 +13,25 @@ export class TrianglespointerComponent implements OnInit {
   ngOnInit(): void {
     
     }
+    fillColor = 'rgb(255, 0, 0)';
 
+    changeColor() {
+      const r = Math.floor(Math.random() * 256);
+      const g = Math.floor(Math.random() * 256);
+      const b = Math.floor(Math.random() * 256);
+      this.fillColor = `rgb(${r}, ${g}, ${b})`;
+    }
  
+    changeColor2() {
+      const colors = ['red', 'blue', 'green', 'orange', 'pink', 'purple']
+          const rando = () => colors[Math.floor(Math.random() * colors.length)];
+          document.documentElement.style.cssText = `
+          --dark-color: ${rando()};
+          --light-color: ${rando()}; 
+          `
+    }
+
+
  
   }
 
